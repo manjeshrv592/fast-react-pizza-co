3ce2dac4fa3bb50c03a5178c7bb9f3715c6bc323
Manjesh R V <manjeshrv592@gmail.com>
2023-10-17 11:26:58 +0530

    finished react router with data loading module

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import { loader as orderLoader } from "./features/order/Order";
+import { action as createOrderAction } from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
